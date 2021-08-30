0655ab38d5c108b0c7c1552fc98bf35ec1b01276
Garrett van Wageningen <ghvanw283@gmail.com>
2021-08-29 22:04:28 -0500

    initish commit

---
+++ src/Elbaremune/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Elbaremune {
+
+    public class Class1 {
+
+    }
+}
---
+++ src/Elbaremune/IEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elbaremune {
+
+    public static class IEnumerableExtensions {
+
+        public static IEnumerable<TState> Scan<TState, TItem>(
+            this IEnumerable<TItem> @this, 
+            TState initialState, 
+            Func<TState, TItem, TState> reducer) {
+
+            var currentState = initialState;
+            foreach (var item in @this) {
+                var newState = reducer(currentState, item);
+                yield return newState;
+                currentState = newState;
+            }
+        }
+    }
+}
